9074d7e34f3753b556b8fced013f23f51e96bf44
romualdc <romuald.caplinskij@stud.techin.lt>
2024-08-22 10:49:12 +0300

    Add GenerateUserData

--- CalculatorPage/src/main/java/lt/techin/calculator/GenerateUserData.java
+++ CalculatorPage/src/main/java/lt/techin/calculator/GenerateUserData.java
@@ -2,6 +2,11 @@ package lt.techin.calculator;
 
 import com.github.javafaker.Faker;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GenerateUserData {
 
     protected Faker faker;
@@ -16,6 +21,31 @@ public class GenerateUserData {
 
     public String getPassword(){
         return faker.internet().password(6,10,true,true,true);
+    }
+
+    public Map<String, String> generateUserDataToMap() {
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", getUsername());
+        userData.put("password", getPassword());
+        return userData;
+    }
+
+    public void saveUserDataToCSV(Map<String, String> userData, String filePath) throws IOException {
+        boolean fileExists = new java.io.File(filePath).exists();
+        //append: true allows to add new lines to the file
+        try (FileWriter fileWriter = new FileWriter(filePath, true)) {
+            if (!fileExists) {
+                //creates header if file does not exist
+                fileWriter.append("username,password\n");
+            }
+            // Write data
+            fileWriter.append(String.join(",",
+                    userData.get("username"),
+                    userData.get("password")
+            ));
+            //appends new data to the new line
+            fileWriter.append("\n");
+        }
     }
 
 }
